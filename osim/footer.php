049c4876c3af06678211ad3074d30815813c9381
manzapo <manzapo@users.noreply.github.com>
2024-01-18 20:04:52 +0700

    Icon untuk CP #2
    - Menambahkan Bootstrap Icons ke assets
    - Implementasi di section Contact Person

--- osim/footer.php
+++ osim/footer.php
@@ -1,12 +1,13 @@
-    <div class="container text-center py-3">
-        <a href="#">Instagram</a>
-        <a href="#">YouTube</a>
-        <a href="#">Email</a>
-    </div>
+	<div class="container text-center py-3">
+		<ul class="list-inline">
+			<li class="list-inline-item"><a href="#" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-body-emphasis px-2"><i class="bi bi-instagram"></i> Instagram</a></li>
+			<li class="list-inline-item"><a href="#" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-body-emphasis px-2"><i class="bi bi-youtube"></i> YouTube</a></li>
+			<li class="list-inline-item"><a href="#" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-body-emphasis px-2"><i class="bi bi-envelope-at"></i> Email</a></li>
+		</ul>
+	</div>
 
-    <script src="./js/bootstrap.bundle.min.js"></script>
-    <script src="./js/holder.min.js"></script>
+	<script src="./js/bootstrap.bundle.min.js"></script>
+	<script src="./js/holder.min.js"></script>
 
 </body>
-
 </html>
