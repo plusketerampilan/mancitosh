f50684bd5bfe3f9f881fb2dfe2fa76d5d7ce1602
anandabbb <157121535+anandabbb@users.noreply.github.com>
2024-01-20 12:14:59 +0700

    hero.php( + link ke lomba.php, +responsive )  memindah poster ke file tersendiri.

--- osim/hero.php
+++ osim/hero.php
@@ -1,10 +1,16 @@
-<div class="container my-5">
-    <div class="row align-items-xl-center">
-        <div class="col-xl-6">
-            <h1 class="py-5 py-xl-0">Pengenalan Mancitosh</h1>
-        </div>
-        <div class="col-xl-6">
-            <img src="https://source.unsplash.com/_XTY6lD8jgM/636x636?robot" class="img-fluid">
-        </div>
+<div class="container">
+<div class="row py-5 d-flex align-items-center">
+    <div class="col-12 col-sm-10 col-md-8 col-lg-6 order-2 order-sm-2 order-md-1">
+        <h1>Call to Action</h1>
+        <p class="lead">
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a
+            paradisematic country, in which roasted parts of sentences fly into your mouth.
+        </p>
+        <p class="mt-4">
+            <a class="btn btn-primary" href="https://www.froala.com">Download</a>
+        </p>
+    </div>
+    <div class="col text-right order-1 order-sm-1 order-md-2">
+        <img src="https://source.unsplash.com/random/570x570?robot" class="img-fluid">
     </div>
 </div>
