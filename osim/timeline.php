a1dd3099d38b386215fb8a87f5f65e1bc7ec4a80
manzapo <manzapo@users.noreply.github.com>
2024-01-22 11:35:40 +0700

    template halaman per lomba

--- osim/timeline.php
+++ osim/timeline.php
@@ -1,18 +1,6 @@
-<div class="container my-5">
-	<h2 class="mb-5">Timeline</h2>
-	<div class="position-relative m-4">
-		<div class="progress" role="progressbar" aria-label="Progress" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 1px;">
-			<div class="progress-bar bg-success" style="width: 100%"></div>
-		</div>
-		<button type="button" class="position-absolute top-0 start-0 translate-middle btn btn-sm btn-success rounded-pill" style="width: 2rem; height:2rem;">1</button>
-		<button type="button" class="position-absolute top-0 start-50 translate-middle btn btn-sm btn-success rounded-pill" style="width: 2rem; height:2rem;">2</button>
-		<button type="button" class="position-absolute top-0 start-100 translate-middle btn btn-sm btn-success rounded-pill" style="width: 2rem; height:2rem;">3</button>
-	</div>
-</div>
-
 <div class="container mt-5 mb-5">
 	<div class="row">
-		<div class="col-md-6 offset-md-3">
+		<div class="col-md-6 border">
 			<h4>Latest News</h4>
 			<ul class="timeline">
 				<li>
